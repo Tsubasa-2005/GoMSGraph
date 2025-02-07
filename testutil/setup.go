08ecad320acf8edfd64a562a2f074dd2fbcee539
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2025-02-07 20:00:08 +0900

    :zap: add testutil SetUpHttpClient

--- testutil/setup.go
+++ testutil/setup.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/Tsubasa-2005/GoMSGraph/graphhelper"
+	"github.com/Tsubasa-2005/GoMSGraph/httpclient"
 )
 
 func SetUpGraphHelper(t *testing.T) *graphhelper.GraphHelper {
@@ -20,4 +21,15 @@ func SetUpGraphHelper(t *testing.T) *graphhelper.GraphHelper {
 		t.Fatalf("GraphHelper の初期化に失敗しました: %v", err)
 	}
 	return gh
+}
+
+func SetUpHttpClient(t *testing.T) *httpclient.Client {
+	baseURL := os.Getenv("GRAPH_API_BASE_URL")
+	gh := SetUpGraphHelper(t)
+	token, err := gh.GetAppToken()
+	if err != nil {
+		t.Fatalf("アクセストークンの取得に失敗しました: %v", err)
+	}
+
+	return httpclient.NewClient(baseURL, token.Token, nil)
 }
